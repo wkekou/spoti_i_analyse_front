d4ccec106b11b9f3e7130aa28eb49e48e2fe06ea
Wilfried EKOU <gilchris70@gmail.com>
2024-05-28 01:16:46 +0200

    Modify Home.js => update spotify data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 const Home = () => {
+  const [setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/update-spotify-data/')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error updating spotify data:', error));
+  });
+
   return (
     <div>
       <h1>Welcome to Spotify Analytics</h1>
